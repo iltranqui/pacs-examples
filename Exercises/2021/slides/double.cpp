33db0b22b8fa9b12a299f605dab4838d327384b4
il.tranqui <enrico.tranquillini@mail.polimi.it>
2021-03-22 11:50:35 +0100

    modify the origin

---
+++ Exercises/2021/slides/double.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <iostream>
+
+std::vector<double>a={3,4,5,6};
+std::vector<double>b;
+
+auto f=[&b](double c){b.emplace_back(c/2.0);};
+auto d=[](double c){std::cout<<c<<"_";};
+
+for (auto i: a)f(i); // fills b
+for (auto i: b)d(i);  // prints b
+// b contains a/2
